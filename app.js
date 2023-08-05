d5db307a77c3577ebc938a5c1a6bb33b793f03d3
deepspatidar6 <deepspatidar8@gmail.com>
2023-08-05 13:44:31 +0530

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app  = express();
+const Routes = require('./config/routes');
+
+
+
+
+// projext-name = POS 
+
+// Password = 'GTeFh5zYwxeykVc1'
+
+app.use(express.static(__dirname+"/assets"));
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cors());
+
+app.use(Routes);
+
+
+const Port = process.env.PORT || 8080 ;
+app.listen(Port, ()=>{
+    console.log(`Server Runing On This Port Number-${Port}`) 
+});
